210751609005c04a1ce60c450ca8fc8bd6f94a97
Marc Christian Encarnacion <91091791@g.mvusd.net>
2021-01-21 19:36:15 +0000

    I changed the image of the duck

--- Main.java
+++ Main.java
@@ -1,60 +1,68 @@
+
 import javax.swing.*;
-import java.awt.event.*;//enables the events
+import java.awt.event.*; //this enables the events
 import java.awt.*;
 
-public class Main 
+
+public class Main
 {
   public static void main (String args[])
   {
-    JFrame frame1 = new JFrame("This is a JFrame");
-    frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    //YOU MUST HAVE THIS LINE
-    frame1.setSize(500,500);
-    frame1.setVisible(true);
+      JFrame frame1 = new JFrame("Rioux Duck Counter");
+      frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //YOU MUST HAVE THIS LINE
+      frame1.setSize(500,500);
+      frame1.setVisible(true);
 
-    //----------Panel to hold the buttons----------
-    JPanel panel1 = new JPanel(new GridLayout(2, 2, 20, 20));
+      //----------Panel to hold the buttons--------
+      JPanel panel1 = new JPanel(new GridLayout(2,2, 20,20));
 
-    //----------Button 1----------
-    JButton button1 = new JButton("Click me!");
+      //---------------Label 1--------------------
+      JLabel label1 = new JLabel("Count the ducks");
+      JTextField textfield1 = new JTextField(10);
 
-    button1.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
+      //-----------------Button 1-------------------
+      JButton  button1 = new JButton("Update Label");
+
+      button1.addActionListener(new ActionListener()
       {
-        System.out.println("Yay! You clicked the button");
-      }//end the block of what the button will do
-    });//end the ActionListener
+          public void actionPerformed (ActionEvent e)
+          {
+            String[] phrases = {"Counting ducks", "You counted to", "Look at you count", "Nice one there", "Keep Going!"};
+            String newText = phrases[(int)(Math.random() * phrases.length)];
+            label1.setText(newText);
+          }//end the block of what the button will do
+      }); //end the ActionListener
 
-    //----------Button 2----------
-    JButton button2 = new JButton("No, click me");
-    button2.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
+      //----------------Button 2 -------------------
+      JButton button2 = new JButton ("No, click me");
+      button2.addActionListener(new ActionListener()
       {
-        System.out.println("Thanks, pal!");
-      }//end the block of what the button will do
-    });//end ActionListener
-
-    //----------Label 1----------
-    JLabel label1 = new JLabel("Count the ducks");
-    JTextField textfield1 = new JTextField(10);
-
-
-    panel1.add(button1);
-    panel1.add(button2);
-    panel1.add(label1);
-    panel1.add(textfield1);
+        public void actionPerformed(ActionEvent e)
+        {
+            System.out.println("Thanks, pal!");
+        }
+      });
+      
 
 
-    //----------Panel 2----------
-    JPanel panel2 = new JPanel (new BorderLayout(10, 10));
-    //----------Image 1 - the DUCK----------
-    ImageIcon duckImg = new ImageIcon("rubberducks.jpg", "A little duck in water");
-    JLabel duckLabelPic = new JLabel(duckImg);
-    panel2.add(duckLabelPic);
+      panel1.add(button1);
+      panel1.add(button2);
+      panel1.add(label1);
+      panel1.add(textfield1);
 
-    frame1.getContentPane().add(BorderLayout.NORTH, panel1);
-    frame1.getContentPane().add(BorderLayout.CENTER, panel2);
+
+      //-----------------Panel 2-----------------
+      JPanel panel2 = new JPanel (new BorderLayout(10,10));
+
+    
+      //--------------Image 1 - the DUCK-------------
+      ImageIcon duckImg = new ImageIcon("duckicon.jpg", "A little duck in water");
+      JLabel duckLabelPic = new JLabel(duckImg);
+      panel2.add(duckLabelPic);
+
+      frame1.getContentPane().add(BorderLayout.NORTH, panel1);
+      frame1.getContentPane().add(BorderLayout.CENTER, panel2);
+ 
   }//end main method
 }//end class Main
